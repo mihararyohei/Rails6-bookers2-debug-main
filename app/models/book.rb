c471bd13a75fb42b07b8eb36e7cd108cb988e311
EC2 Default User <108612635+mihararyohei@users.noreply.github.com>
2022-07-25 11:10:49 +0000

    応用課題２

---
+++ app/models/book.rb
@@ -0,0 +1,9 @@
+class Book < ApplicationRecord
+   belongs_to :user
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
+
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+end
